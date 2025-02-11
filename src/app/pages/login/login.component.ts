114631af8be58516553e5661756085a6a4af0738
karolynelima <kmrlima36@gmail.com>
2025-02-10 22:46:48 -0300

    feat: head component creation

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,15 +2,17 @@ import { Component } from '@angular/core';
 import { DefaultLoginLayoutComponent } from '../../components/default-login-layout/default-login-layout.component';
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { PrimaryInputsComponent } from '../../components/primary-inputs/primary-inputs.component';
+import { HeaderComponent } from "../../components/header/header.component";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ //components import
+  imports: [
     DefaultLoginLayoutComponent,
     ReactiveFormsModule,
     PrimaryInputsComponent,
-  ],
+    HeaderComponent
+],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
